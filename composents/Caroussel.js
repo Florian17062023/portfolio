5e6c2f124a0443898ae67713e572a2a5bf852826
Florian <florian.serrault@hotmail.fr>
2024-06-03 01:11:48 +0200

    homepage

--- composents/Caroussel.js
+++ composents/Caroussel.js
@@ -4,29 +4,55 @@ import styles from '../styles/caroussel.module.css';
 
 const Caroussel = () => {
   return (
-    <div className={styles.carouselContainer}> 
-      <CCarousel controls indicators dark>
-        <CCarouselItem>
-          <CImage
-            className={styles.carouselImage}
-            src={'/panda.jpg'}
-            alt="slide 1"
-          />
-          <CCarouselCaption className="d-none d-md-block">
-            
-          </CCarouselCaption>
-        </CCarouselItem>
-        <CCarouselItem>
-          <CImage
-            className={styles.carouselImage}
-            src={'/vache.jpg'}
-            alt="slide 2"
-          />
-          <CCarouselCaption className="d-none d-md-block">
-            
-          </CCarouselCaption>
-        </CCarouselItem>
-      </CCarousel>
+    <div className={styles.container}>
+      <div className={styles.textContent}>
+        <h2 className={styles.carousselTitle}>Chels_arts88</h2>
+        <p className={styles.carousselDescription}>Texte d'explication du carrousel</p>
+      </div>
+      <div className={styles.carouselContainer}>
+        <CCarousel controls indicators dark>
+          <CCarouselItem>
+            <CImage
+              className={styles.carouselImage}
+              src={'/animalcoeur.jpg'}
+              alt="slide 1"
+            />
+            <CCarouselCaption className="d-none d-md-block">
+              
+            </CCarouselCaption>
+          </CCarouselItem>
+          <CCarouselItem>
+            <CImage
+              className={styles.carouselImage}
+              src={'/vache.jpg'}
+              alt="slide 2"
+            />
+            <CCarouselCaption className="d-none d-md-block">
+              
+            </CCarouselCaption>
+          </CCarouselItem>
+          <CCarouselItem>
+            <CImage
+              className={styles.carouselImage}
+              src={'/lapin.jpg'}
+              alt="slide 3"
+            />
+            <CCarouselCaption className="d-none d-md-block">
+              
+            </CCarouselCaption>
+          </CCarouselItem>
+          <CCarouselItem>
+            <CImage
+              className={styles.carouselImage}
+              src={'/vachenoire.jpg'}
+              alt="slide 4"
+            />
+            <CCarouselCaption className="d-none d-md-block">
+              
+            </CCarouselCaption>
+          </CCarouselItem>
+        </CCarousel>
+      </div>
     </div>
   );
 };
