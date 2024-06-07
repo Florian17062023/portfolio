7a4b3648a609843ac24e950806421ae0d4dc89d7
Florian <florian.serrault@hotmail.fr>
2024-06-07 02:34:42 +0200

    portfolio v4

--- composents/About.js
+++ composents/About.js
@@ -1,6 +1,5 @@
-
-import React from 'react';
-import styles from '../styles/about.module.css'
+import React from "react";
+import styles from "../styles/about.module.css";
 
 const About = () => {
   return (
@@ -8,9 +7,11 @@ const About = () => {
       <div className={styles.leftContent}>
         <h2 className={styles.title}>About</h2>
         <p className={styles.paragraph}>
-        Ayant développé une passion pour le développement depuis ces derniers mois, je cherche une première expérience professionnelle pour mettre en pratique les compétences que j'ai acquises lors de ces derniers mois et apprendre le métier de développeur web et mobile.
+          Ayant développé une passion pour le développement depuis ces dernières
+          années, je cherche une première expérience professionnelle pour mettre
+          en pratique les compétences que j'ai acquises lors de ces derniers
+          mois et apprendre le métier de développeur web et mobile.
         </p>
-        
       </div>
       <div className={styles.rightContent}>
         <img src="/profiles.jpg" alt="Profile" />
